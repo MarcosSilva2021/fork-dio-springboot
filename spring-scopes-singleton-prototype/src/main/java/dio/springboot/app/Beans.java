573368c02a4be597207eb21712ef8f7830bfba6d
Marcos Silva <89165879+MarcosSilva2021@users.noreply.github.com>
2024-10-09 19:34:02 -0300

    add comentário prototype

--- spring-scopes-singleton-prototype/src/main/java/dio/springboot/app/Beans.java
+++ spring-scopes-singleton-prototype/src/main/java/dio/springboot/app/Beans.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class Beans {
     @Bean
-    @Scope("prototype")
+    @Scope("prototype")// definou como prototype, se não houvesse seria sington
     public Remetente remetente(){
         System.out.println("CRIANDO UM OBJETO REMETENTE");
         Remetente remetente = new Remetente();
