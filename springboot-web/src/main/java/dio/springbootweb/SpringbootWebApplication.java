69c3fcf38d27c05bb1875cc34e84e6d6eb10b1e5
Marcosdsj <mfsilva5585@gmail.com>
2024-10-14 21:07:49 -0300

    add imports

--- springboot-web/src/main/java/dio/springbootweb/SpringbootWebApplication.java
+++ springboot-web/src/main/java/dio/springbootweb/SpringbootWebApplication.java
@@ -2,6 +2,7 @@ package dio.springbootweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import controller/UsuarioController;
 
 @SpringBootApplication
 public class SpringbootWebApplication {
--- springboot-web/src/main/java/dio/springbootweb/controller/UsuarioController.java
+++ springboot-web/src/main/java/dio/springbootweb/controller/UsuarioController.java
@@ -3,7 +3,13 @@ package dio.springbootweb.controller;
 import dio.springbootweb.model.Usuario;
 import dio.springbootweb.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
